e220c3c9256a5c0c63501e8548b9bbe12639d357
lidebao <lidebao@gw.com.cn>
2015-02-06 16:25:09 +0800

    //使用“/”，win与linux通用 。

--- AccountV3Test/src/main/java/com/atopcloud/util/MyConfigUtil.java
+++ AccountV3Test/src/main/java/com/atopcloud/util/MyConfigUtil.java
@@ -8,7 +8,7 @@ package com.atopcloud.util;
 public class MyConfigUtil {	
 	public static String getConfig(String key){
 		//读取环境变量
-		String envPath = System.getProperty("user.dir") + "\\config\\env.properties";
+		String envPath = System.getProperty("user.dir") + "/config/env.properties";   //使用“/”，win与linux通用 。
 		PropertiesManager pm = new PropertiesManager(envPath);
 //		 ip = pm.getValue("ip");
 //		 port=pm.getValue("port");
